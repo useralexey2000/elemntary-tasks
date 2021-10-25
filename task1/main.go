45a23e18167d6312e6092d0e7e47d28a3c013082
useralexey2000 <useralexey2000@gmail.com>
2021-10-26 00:10:19 +0300

    Modifying task1 for testing

--- task1/main.go
+++ task1/main.go
@@ -1,37 +1,30 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"os"
 	"strconv"
 )
 
 func main() {
-
-	args := os.Args
-	if len(args) < 3 {
-		usage(args[0])
-		os.Exit(1)
-	}
-
-	w, err := strconv.Atoi(args[1])
+	w, h, err := readArgs()
 	if err != nil {
-		usage(args[0])
+		fmt.Println(err)
+		usage(os.Args[0])
 		os.Exit(1)
 	}
-	h, err := strconv.Atoi(args[2])
-	if err != nil {
-		usage(args[0])
-		os.Exit(1)
-	}
-	checks(w, h)
+
+	arr := checks(w, h)
+	print(arr)
 }
 
 func usage(n string) {
 	fmt.Printf("usage: %v %v %v\n", n, "width<int>", "height<int>")
 }
 
-func checks(w, h int) {
+func checks(w, h int) [][]string {
+	outer := make([][]string, 0)
 	for i := 0; i < h; i++ {
 		var pattern string
 		if i%2 > 0 {
@@ -39,9 +32,38 @@ func checks(w, h int) {
 		} else {
 			pattern = "* "
 		}
+		inner := make([]string, 0)
 		for j := 0; j < w; j++ {
-			fmt.Print(pattern)
+			inner = append(inner, pattern)
+
+		}
+		outer = append(outer, inner)
+	}
+	return outer
+}
+
+func print(arr [][]string) {
+	for _, o := range arr {
+		for _, i := range o {
+			fmt.Print(i)
 		}
 		fmt.Println()
 	}
+}
+
+func readArgs() (int, int, error) {
+	args := os.Args
+	if len(args) < 3 {
+		return 0, 0, errors.New("Arguments are < 3")
+	}
+
+	w, err := strconv.Atoi(args[1])
+	if err != nil {
+		return 0, 0, err
+	}
+	h, err := strconv.Atoi(args[2])
+	if err != nil {
+		return 0, 0, err
+	}
+	return w, h, nil
 }
