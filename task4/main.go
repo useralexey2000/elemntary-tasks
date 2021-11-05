138fcff1a00a5ede260f422f8778f64de18df22a
useralexey2000 <useralexey2000@gmail.com>
2021-11-05 07:31:11 +0200

    refactoring task4 compleating testing for task4

--- task4/main.go
+++ task4/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"io/ioutil"
 	"os"
 	"strings"
@@ -10,14 +11,21 @@ import (
 func main() {
 
 	args := os.Args
-	if len(args) < 3 {
+	if len(args) < 3 || len(args) > 4 {
 		usage(args[0])
 		os.Exit(0)
 
 	}
+	// can be separated for reading and writing depending on method called
+	f, err := os.OpenFile(args[1], os.O_RDWR, 066)
+	if err != nil {
+		fmt.Printf("cant open file %s, %v\n", args[1], err)
+		os.Exit(1)
+	}
+
 	if len(args) == 3 {
 
-		i, err := countString(args[1], args[2])
+		i, err := CountString(f, args[2])
 		if err != nil {
 			fmt.Println(err)
 		}
@@ -26,33 +34,56 @@ func main() {
 
 	}
 	if len(args) == 4 {
-		if err := replaceString(args[1], args[2], args[3]); err != nil {
+
+		if err := ReplaceString(&MyFile{f: f}, args[2], args[3]); err != nil {
 			fmt.Println(err)
 		}
 		os.Exit(0)
 	}
 }
 
-func countString(f string, s string) (int, error) {
-	bs, err := ioutil.ReadFile(f)
+func CountString(in io.Reader, s string) (int, error) {
+
+	bs, err := ioutil.ReadAll(in)
 	if err != nil {
-		return 0, fmt.Errorf("Cant read file %v", err)
+		return 0, fmt.Errorf("cant read file %w", err)
 	}
 	i := strings.Count(string(bs), s)
 
 	return i, nil
 }
 
-func replaceString(f, s1, s2 string) error {
-	bs, err := ioutil.ReadFile(f)
+type MyFile struct {
+	f *os.File
+}
+
+// overwrite write method for os.file to overwrite file and compy with io.ReadWriter
+// no need to test them as they are direct calls to methods in standard library
+func (ff *MyFile) Write(b []byte) (int, error) {
+	return ff.f.WriteAt(b, 0)
+}
+
+func (ff *MyFile) Read(b []byte) (int, error) {
+	return ff.f.Read(b)
+}
+
+func ReplaceString(rw io.ReadWriter, s1, s2 string) error {
+
+	bs, err := ioutil.ReadAll(rw)
+
 	if err != nil {
-		return fmt.Errorf("Cant read file %v", err)
+		return fmt.Errorf("cant read from source %w", err)
 	}
-	s := strings.ReplaceAll(string(bs), s1, s2)
-	if err = ioutil.WriteFile(f, []byte(s), 066); err != nil {
-		return fmt.Errorf("Cant read file %v", err)
+
+	res := strings.ReplaceAll(string(bs), s1, s2)
+
+	_, err = rw.Write([]byte(res))
+	if err != nil {
+		return fmt.Errorf("cant read file %w", err)
 	}
+
 	return nil
+
 }
 
 func usage(n string) {
